d237bcd09594c20a810aef6185396c9bac20e96a
Yume <yume190@gmail.com>
2022-08-03 15:56:45 +0800

    fix custom operator. accessor

--- Sources/RuleBuilder/Property+Operator.swift
+++ Sources/RuleBuilder/Property+Operator.swift
@@ -9,15 +9,15 @@ import Foundation
 
 infix operator => : AssignmentPrecedence
 extension String {
-    static func =>(propertyName: String, @LabelBuilder builder: () -> [LabelBuilder.Target]) -> Property {
+    public static func =>(propertyName: String, @LabelBuilder builder: () -> [LabelBuilder.Target]) -> Property {
         .init(propertyName, builder: builder)
     }
     
-    static func =>(propertyName: String, label: Label) -> Property {
+    public static func =>(propertyName: String, label: Label) -> Property {
         .init(propertyName, labels: [label])
     }
     
-    static func =>(propertyName: String, labels: [Label]) -> Property {
+    public static func =>(propertyName: String, labels: [Label]) -> Property {
         .init(propertyName, labels: labels)
     }
 }
